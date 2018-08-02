76c1994d7de3b6492627248e91eb64a8d61fdd8c
Abhishek <avelay@terpmail.umd.edu>
2018-08-01 22:42:45 -0400

    small changes (formatting)

--- src/app/models/DataTypes.ts
+++ src/app/models/DataTypes.ts
@@ -1,20 +1,20 @@
 export interface ClassData {
   ID: string;
-  name : string;
-  studyGroupIDs?: {[studyGroupID: string] : boolean};
+  name: string;
+  studyGroupIDs?: {[studyGroupID: string]: boolean};
 }
 
 export interface StudyGroup {
   ID: string;
-  ClassID? : string;
-  name : string;
-  description? : string;
-  usersList? : string[];
-  userMessageList? : string[];
+  ClassID?: string;
+  name: string;
+  description?: string;
+  usersList?: string[];
+  userMessageList?: string[];
 }
 
 export interface User {
-  username : string;
-  email : string;
+  username: string;
+  email: string;
   groupIDs: string[];
 }
