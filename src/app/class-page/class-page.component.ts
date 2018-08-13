f80ca9c3d89628f99f647cc0b3fae659ead867e5
PuneethBikkumanla <pbikkumanla@gmail.com>
2018-08-13 01:26:32 -0400

    finished dynamic pagination for classpage

--- src/app/class-page/class-page.component.ts
+++ src/app/class-page/class-page.component.ts
@@ -11,24 +11,42 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./class-page.component.css']
 })
 export class ClassPageComponent implements OnInit {
-  maxSize = 5;
-  bigTotalItems = 175;
+  maxSize = 4;
+  bigTotalItems: number;
   bigCurrentPage = 1;
+  numItemsPerPage = 5;
   // className: string;
 
-  // className = this.transferService.getData();
   className = 'CMSC 120';
+  allGroups: StudyGroup[];
+  currGroups: StudyGroup[];
 
-  groups: StudyGroup[];
-  constructor(private studyGroupService: StudyGroupService, private transferService: TransferService,
-    private route: ActivatedRoute) { }
+  constructor(private studyGroupService: StudyGroupService, private route: ActivatedRoute) { }
 
   async ngOnInit() {
     this.route.params.subscribe(async params => {
       // this.className = params['classname'];
-      this.groups = await this.studyGroupService.getClassStudyGroups(this.className);
-      console.log(this.groups);
+      this.allGroups = await this.studyGroupService.getClassStudyGroups(this.className);
+      this.bigTotalItems = this.numOfPages(this.allGroups.length);
+      this.currGroups = this.pageGroups(1, this.numItemsPerPage);
     });
+  }
+
+  numOfPages(len: number): number {
+    if (len % this.numItemsPerPage === 0) {
+      return len / this.numItemsPerPage;
+    } else {
+      return (len / this.numItemsPerPage) + 1;
+    }
+  }
+
+  pageGroups(page_num: number, numItems: number): StudyGroup[] {
+    --page_num;
+    return this.allGroups.slice(page_num * numItems, (page_num + 1) * numItems);
+  }
+
+  pageChanged(event) {
+    this.currGroups = this.pageGroups(event.page, event.itemsPerPage);
   }
 
 }
